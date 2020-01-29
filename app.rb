e8051cda9916cfa8df9edf771240700083761986
niklas <niklas.enns@gmail.com>
2020-01-29 11:26:20 +0100

    refac: use global variable for stocks

--- app.rb
+++ app.rb
@@ -2,33 +2,34 @@ def import
     require "json"
     puts "Importing import.json"
     file = File.open "import.json"
-    data = JSON.load file
+    $inventar= JSON.load file
 end
 
 
-def anzeige(inventar)
+def anzeige()
     puts "Inventar"
     puts \
         "inventar: \
-            #{inventar['tshirts']} tshirts\
-            #{inventar['pullover']} pullover\
-            #{inventar['hemden']} hemden\
-            #{inventar['jacken']} jacken\
-            #{inventar['hosen']} hosen\
+            #{$inventar['tshirts']} tshirts\
+            #{$inventar['pullover']} pullover\
+            #{$inventar['hemden']} hemden\
+            #{$inventar['jacken']} jacken\
+            #{$inventar['hosen']} hosen\
         "
 end
-def hinzufügen(inventar)
+def hinzufügen()
     puts "Was darf's denn sein? Ich kann die Anzahl von Tshirts, Pullover, Hemden, Jacken und Hosen ändern"
     choice = gets.strip
     puts "Ok, wir ändern die Anzahl von #{choice}. Um wieviele Elemente?"
     count = gets.strip.to_i
     operation = count > 1 ? "hinzugefügt" : "entfernt" 
     puts "Okese, #{count} #{choice} wurden #{operation}"
-    new_count = inventar[choice] + count
-    puts "Aus #{inventar[choice]} #{choice} werden jetzt #{new_count}"
-    inventar[choice] = new_count 
+    new_count = $inventar[choice] + count
+    puts "Aus #{$inventar[choice]} #{choice} werden jetzt #{new_count}"
+    $inventar[choice] = new_count 
 end
 
+import
 puts \
     "Hauptmenü. \n\
     1 - Inventar anzeigen \n\
@@ -37,7 +38,7 @@ puts \
 choice = gets.strip
 case choice
 when "1"
-    anzeige import
+    anzeige 
 when "2"
-    hinzufügen import
+    hinzufügen 
 end
